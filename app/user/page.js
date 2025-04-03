ee6ec4c1906d32d32f96f8f363e9f8ab18b28e1b
yvan-dz <yvandzefak1@gmail.com>
2025-04-03 21:10:07 +0200

    Elint and user router modified

--- app/user/page.js
+++ app/user/page.js
@@ -30,7 +30,8 @@ export default function UserPage() {
       }
     })
     return () => unsubscribe()
-  }, [])
+  }, [router])
+  
 
   const handlePasswordChange = async () => {
     setMessage("")
